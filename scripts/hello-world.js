75f54d43526a55811a12d8aba3038070596cb59e
Martin Naumann <martin.naumann@centralway.com>
2012-10-30 07:55:11 +0100

    Now with refresh_token stuff to keep access

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -3,6 +3,8 @@
 var google_client_id = "";
 var google_client_secret = "";
 var google_api_key = "";
+//Automatically fetched:
+var google_refresh_token = null;
 var google_token = null;
 
 var calendars = [
@@ -27,27 +29,54 @@ function onDeviceReady() {
     //When the callback URL is loading, we jump in and grab the code from the URL
     if(!window.plugins || !window.plugins.childBrowser) ChildBrowser.install();
     window.plugins.childBrowser.onLocationChange = function(returnUrl) {
-        var code = returnUrl.match(/access_token=([^&]*)/);
+        var code = returnUrl.match(/code=([^&]*)/);
         if(code[1] != undefined) {
-            google_token = code[1];
+          getTokensFromCode(code[1]);
           window.plugins.childBrowser.close(); //Close the useless window and grab all the calendars!
-          for(var i=0, len = calendars.length; i < len; i++) {
-            getCalendarEvents(calendars[i], i, google_token);
-          }
         }
     };
     
-    var url = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/calendar/feeds/&response_type=token&redirect_uri=http://www.geekonaut.de/&client_id=" + google_client_id;
+    var url = "https://accounts.google.com/o/oauth2/auth?scope=https://www.google.com/calendar/feeds/&response_type=code&redirect_uri=http://www.geekonaut.de/&access_type=offline&approval_prompt=force&client_id=" + google_client_id;
     window.plugins.childBrowser.showWebPage(url);
 
 }
 
-function getCalendarEvents(calendar, index, token) {
+function getTokensFromCode(code) {
+    $.post("https://accounts.google.com/o/oauth2/token", {
+            "code": code,
+            "client_id": google_client_id,
+            "client_secret": google_client_secret,
+            "redirect_uri": "http://www.geekonaut.de/",
+            "grant_type": "authorization_code"
+        }, function(result) {
+            google_token = result.access_token;
+            google_refresh_token = result.refresh_token;
+            setTimeout("getNewAccessToken()", (result.expires_in - 10) * 1000);
+            
+            for(var i=0, len = calendars.length; i < len; i++) {
+                getCalendarEvents(calendars[i], i, google_token);
+            }
+    });
+}
+
+function getNewAccessToken() {
+    $.post("https://accounts.google.com/o/oauth2/token", {
+            "refresh_token": google_refresh_token,
+            "client_id": google_client_id,
+            "client_secret": google_client_secret,
+            "grant_type": "refresh_token"
+        }, function(result) {
+            google_token = result.access_token;
+            setTimeout("getNewAccessToken()", (result.expires_in - 10) * 1000);
+        }).error(function(xhr) { alert("ERROR: Cannot refresh the access token (" + xhr.responseText + ")") });    
+}
+
+function getCalendarEvents(calendar, index) {
     //Get only the next or currently running event
     var now = new Date();
     var dateTimeString = now.getFullYear() +"-"+ (now.getMonth()+1) + "-" + now.getDate() +"T"+ now.getHours() + ":" + now.getMinutes() + ":00+01:00" ;
     $.ajax({
-        url: "https://www.googleapis.com/calendar/v3/calendars/" + calendar + "/events?access_token=" + token,
+        url: "https://www.googleapis.com/calendar/v3/calendars/" + calendar + "/events?access_token=" + google_token,
         datatype : "json",
         data: {
             "orderBy": "startTime",
@@ -75,6 +104,6 @@ function getCalendarEvents(calendar, index, token) {
             $("#tabstrip-calendar" + index + " .eventTime").text(timeString);
         }
     }).fail(function(xhr, complaint) { alert("ERROR:" + xhr.responseText + " D:"); });
-
-    setTimeout(function() { getCalendarEvents(calendar, index, token); }, 60000);
+    
+    setTimeout(function() { getCalendarEvents(calendar, index); }, 60000);
 }
